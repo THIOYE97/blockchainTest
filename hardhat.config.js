ad46078c631cdb493c2e30d0039b808a5db772f7
almamysamina <almamy.haidara@samina.tech>
2024-02-23 11:10:22 +0000

    last

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+const { INFURA_ENDPOINT, MNEMONIC } = process.env;
 
+const INFURA_API_KEY = "48a84d52f69549348cdc9b3d11c2feca"
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.11",
@@ -8,6 +11,11 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    sepolia: {
+      url: INFURA_ENDPOINT,
+      chainId:11155111,
+      accounts: ["ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
     },
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,10 @@
 const hre = require("hardhat");
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  const token = await hre.ethers.deployContract("Vote");
+  await token.waitForDeployment();
 
-  console.log("Déploiement des contrats avec le compte:", deployer.address);
-  const Vote = await hre.ethers.getContractFactory("Vote");
-  const token = await Vote.deploy();
-
-  // Attendre que le contrat soit déployé
-  await token.deployed();
-
-  // Attendre que la transaction de déploiement soit confirmée
-  await token.deployTransaction.wait();
-
-  console.log("Contrat déployé à l'adresse:", token.address);
+  console.log("Donations contract deployed at:", token.target);
 }
 
 main()
