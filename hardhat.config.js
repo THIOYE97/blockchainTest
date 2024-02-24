7db4fd393cd1fedcea8a7c4e4d52ee7c3fa65d9f
THIOYE MARIKO <thioyenouhoummariko@gmail.com>
2024-02-24 11:04:22 +0000

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,3 +19,28 @@ module.exports = {
     },
   },
 };
+
+
+ma proposition ici :
+Tu supprimes en haut
+
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+
+const INFURA_HTTP_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: INFURA_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+};
